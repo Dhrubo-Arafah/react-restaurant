52e16c4934127f409173967bdc1f390a2d50129c
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-20 16:14:00 +0600

    fixing routing problem V3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <BrowserRouter>
+        <BrowserRouter basename="/react-restaurant">
           <MainComponent />
         </BrowserRouter>
       </div>
