077ff5411686d8fa9fc29ef92bc10150ccf32099
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-22 10:19:09 +0600

    react-redux-form validation

--- src/component/body/Contact.js
+++ src/component/body/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Button, FormGroup, Label, Col } from 'reactstrap';
 import { Form, Control, Errors, actions } from 'react-redux-form';
 import { connect } from 'react-redux';
@@ -11,8 +11,8 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-const required = val = val && val.length;
-const isNum = val => !isNaN(Number(val));
+const required = val => val && val.length;
+const isNumber = val => !isNaN(Number(val));
 const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
@@ -20,16 +20,17 @@ class Contact extends Component {
     console.log(values);
     this.props.resetFeedbackForm();
   }
+
   render() {
+    document.title = "Contact";
     return (
       <div className="container">
-        <div className="row row-content" style={{ paddingLeft: "20", textAlign: "left" }}>
-          <div className="col-12 col-md-7">
-            <h3>Send us your FeedBack</h3>
-          </div>
+        <div className="row row-content" style={{ paddingLeft: "20px", textAlign: "left" }}>
           <div className="col-12">
-            <Form model=".feedback"
-              onSubmit={values => this.handleSubmit(values)}>
+            <h3>Send us your Feedback</h3>
+          </div>
+          <div className="col-12 col-md-7">
+            <Form model="feedback" onSubmit={values => this.handleSubmit(values)}>
               <FormGroup row>
                 <Label htmlFor="firstname" md={2}>First Name</Label>
                 <Col md={10}>
@@ -46,9 +47,11 @@ class Contact extends Component {
                     className="text-danger"
                     model=".firstname"
                     show="touched"
-                    message={{
-                      required: "Required"
-                    }}
+                    messages={
+                      {
+                        required: "Required"
+                      }
+                    }
                   />
                 </Col>
               </FormGroup>
@@ -68,9 +71,11 @@ class Contact extends Component {
                     className="text-danger"
                     model=".lastname"
                     show="touched"
-                    message={{
-                      required: "Required"
-                    }}
+                    messages={
+                      {
+                        required: "Required"
+                      }
+                    }
                   />
                 </Col>
               </FormGroup>
@@ -83,22 +88,25 @@ class Contact extends Component {
                     placeholder="Tel. Number"
                     className="form-control"
                     validators={{
-                      required, isNum
+                      required,
+                      isNumber
                     }}
                   />
                   <Errors
                     className="text-danger"
                     model=".telnum"
                     show="touched"
-                    message={{
-                      required: "Required, ",
-                      isNum: "Invalid Number"
-                    }}
+                    messages={
+                      {
+                        required: "Required, ",
+                        isNumber: "Invalid Number!"
+                      }
+                    }
                   />
                 </Col>
               </FormGroup>
               <FormGroup row>
-                <Label htmlFor="email" md={2}>Email Address</Label>
+                <Label htmlFor="email" md={2}>Email</Label>
                 <Col md={10}>
                   <Control.text
                     model=".email"
@@ -112,12 +120,14 @@ class Contact extends Component {
                   />
                   <Errors
                     className="text-danger"
-                    model=".firstname"
+                    model=".email"
                     show="touched"
-                    message={{
-                      required: "Required, ",
-                      validEmail: "Invalid Email"
-                    }}
+                    messages={
+                      {
+                        required: "Required, ",
+                        validEmail: "Invalid Email!"
+                      }
+                    }
                   />
                 </Col>
               </FormGroup>
@@ -130,7 +140,8 @@ class Contact extends Component {
                         name="agree"
                         className="form-check-input"
                       />
-                      <strong>May We Contact You?</strong>
+                      <strong>May we contact you?
+                                            </strong>
                     </Label>
                   </FormGroup>
                 </Col>
@@ -148,29 +159,39 @@ class Contact extends Component {
                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                 <Col md={10}>
                   <Control.textarea
-                    model=".message"
                     name="message"
-                    placeholder="Your Message"
+                    model=".message"
                     rows="12"
                     className="form-control"
                     validators={{
                       required
-                    }}>
-                  </Control.textarea>
+                    }}
+                  />
                 </Col>
+                <Errors
+                  className="text-danger"
+                  model=".message"
+                  show="touched"
+                  messages={
+                    {
+                      required: "Required"
+                    }
+                  }
+                />
               </FormGroup>
               <FormGroup>
                 <Col md={{ size: 10, offset: 2 }}>
                   <Button type="submit" color="primary">
-                    Send FeedBack
-                  </Button>
+                    Send Feedback
+                                    </Button>
                 </Col>
               </FormGroup>
             </Form>
           </div>
         </div>
-      </div >
-    )
+      </div>
+    );
   }
 }
-export default connect(null, mapDispatchToProps)(Contact)
+
+export default connect(null, mapDispatchToProps)(Contact);
